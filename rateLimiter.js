a7da8fbded335ab0fde7b286596651de0bcd509d
harnaman-hk <harnaman.hk@gmail.com>
2023-06-16 14:07:02 +0530

    rate limiter logic using hash map

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,10 +1,31 @@
 const defaults = {
+    routePrefix: "",
+    maxRequestsPerSecond: 2,
     blockRequestStatusCode: 429,
     blockRequestMessage: "Rate Limit Exceeded. Please try again later.",
 }
 
-function RateLimiter(req, res, next) {
-    console.log("ratelimiter is called")
+const memory = new Map();
+
+function RateLimiter(request, response, next) {
+    const ipAddress = request.ip;
+    const timestamp = Math.round(Date.now() / 1000);
+    const key = defaults.routePrefix + ipAddress;
+    const store = memory.get(key);
+
+    if (store) {
+        const counter = store[timestamp] ?? 0;
+        if (counter >= defaults.maxRequestsPerSecond) {
+            return response.status(defaults.blockRequestStatusCode).send({
+                message: defaults.blockRequestMessage
+            });
+        } else {
+            store[timestamp] = counter + 1;
+        }
+    } else {
+        memory.set(key, {[timestamp]: 1});
+    }
+    
     next();
 }
 
